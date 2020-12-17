debbe8116c916404902b63374fb8cee054e9ffe6
evgenii <evgenii@adsquare.com>
2020-12-17 13:10:16 +0100

    refactor

--- src/main/scala/webapp/Game.scala
+++ src/main/scala/webapp/Game.scala
@@ -1,27 +1,17 @@
 package webapp
 
-import webapp.Game.size
-
 import scala.util.Random
 
-case class Game(cells: Seq[Row], score: Int){
+case class Game(cells: Board, score: Int) {
 
   def shift(dir: Direction): Option[Game] = {
-    val rotated = dir match {
-      case Up => Rotations.deg90(cells)
-      case Down =>  Rotations.minus90(cells)
-      case Left => cells
-      case Right => Rotations.deg180(cells)
-    }
+    val (rotation, backRotation) = Rotations.chooseRotations(dir)
+
+    val rotated = rotation(cells)
 
     val res = Game(rotated, -1).shiftLeft().map(g => {
-      val back = dir match {
-        case Up => Rotations.deg90(g.cells)
-        case Down =>  Rotations.minus90back(g.cells)
-        case Left => g.cells
-        case Right => Rotations.deg180(g.cells)
-      }
-      Game(back, g.score)
+      val andBack = backRotation(g.cells)
+      Game(andBack, g.score)
     })
 
     res
@@ -30,30 +20,48 @@ case class Game(cells: Seq[Row], score: Int){
   def shiftLeft(): Option[Game] = {
     val res = cells.map(row => {
       val (shifted, points) = ShiftUtil.shiftLeft(row)
-      val padded = shifted.map(Some(_)).padTo(size, None)
+      val padded = shifted.map(Some(_)).padTo(boardSize, None)
       val changed = (padded != row)
 
       (ShiftUtil.addNewCellIfCan(padded), changed, points)
     })
 
-    if(res.exists(_._2)){
+    if (res.exists(_._2)) {
       val totalpoints = score + res.map(_._3).sum
       Some(Game(res.map(_._1), totalpoints))
     } else None
   }
 }
 
-object Rotations{
-  def deg90(rows: Seq[Row]): Seq[Row] = rows.transpose
-  def deg180(rows: Seq[Row]): Seq[Row] = rows.map(_.reverse)
-  def minus90(rows: Seq[Row]): Seq[Row] = rows.transpose.map(_.reverse)
-  def minus90back(rows: Seq[Row]): Seq[Row] = rows.map(_.reverse).transpose
+object Rotations {
+  type Rotation = Board => Board
+
+  val rightRotation: Rotation = b => b.transpose
+  val reverseRightRotation: Rotation = rightRotation
+
+  val leftRotation: Rotation = b => b.transpose.map(_.reverse)
+  val reverseLeftRotation: Rotation = b => b.map(_.reverse).transpose
+
+  val revert: Rotation = b => b.map(_.reverse)
+
+  def chooseRotations(direction: Direction): (Rotation, Rotation) = {
+    direction match {
+      case Up => (rightRotation, reverseRightRotation)
+      case Down => (leftRotation, reverseLeftRotation)
+      case Left => (identity, identity)
+      case Right => (revert, revert)
+    }
+  }
 }
 
 sealed trait Direction
+
 case object Up extends Direction
+
 case object Down extends Direction
+
 case object Left extends Direction
+
 case object Right extends Direction
 
 object Game {
@@ -70,10 +78,7 @@ object Game {
 
 
   def nextNum(): Int = {
-    if(rand.nextBoolean()) 4 else 2
+    if (rand.nextBoolean()) 4 else 2
   }
-
-  val size: Int = 4
-
 
 }
--- src/main/scala/webapp/package.scala
+++ src/main/scala/webapp/package.scala
@@ -1,3 +1,5 @@
 package object webapp {
   type Row = Seq[Option[Int]]
+  type Board = Seq[Row]
+  val boardSize: Int = 4
 }
